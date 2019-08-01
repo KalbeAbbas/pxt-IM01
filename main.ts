cfb2e4b70c34e62fd867d54507a2c7c312606e24
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-01 19:33:41 +0500

    BM11 RGB LED blink

--- main.ts
+++ main.ts
@@ -1,7 +1,28 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=50 color=#081620 icon="O" block="BM11_led"
+namespace BM11_led {
+
+    export enum LED {
+        //% block="GREEN"
+        GREEN = 0,
+        //% block="BLUE"
+        BLUE = 1,
+    }
+
+    //% blockId="blink_led" block="Blink LED %led| with period of %interval| ms"
+    //% weight=30 blockGap=8
+    export function blink_led(led: LED, interval: number) {
+        if (led == LED.GREEN) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            basic.pause(interval)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            basic.pause(interval)
+        }
+
+        if (led == LED.BLUE) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(interval)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            basic.pause(interval)
+        }
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,3 @@
-// tests go here; this will not be compiled when this package is used as a library
+basic.forever(function () {
+    BM11_led.blink_led(BM11_led.LED.GREEN, 1000)
+})
