6184fab29781a0ff9480eba412c3c5d4b54d3f31
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-05 20:39:05 +0500

    New Block Added

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 //% weight=50 color=#081620 icon="O" block="BM11_led"
 namespace BM11_led {
 
+    export enum STATE {
+        //% block="TRUE"
+        TRUE = 0,
+        //% block="FALSE"
+        FALSE = 1,
+    }
+
     export enum LED {
         //% block="RED"
         RED = 0,
@@ -20,6 +27,60 @@ namespace BM11_led {
         BROWN = 7,
         //% block="BLACK"
         BLACK = 8,
+    }
+
+    //% blockId="show_color_on_led" block="Turn On %led| with intensity level %intensity| %state=1"
+    //% weight=30 blockGap=8
+    //%intensity.min=1 intensity.max=10
+    export function Show_Color_On_LED(led: LED, intensity: number, state: STATE) {
+        
+        if (led == LED.RED) {
+            pins.analogWritePin(AnalogPin.P2, pins.map(255 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(0, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(0, 0, 1023, 0, 255))
+        }
+
+        if (led == LED.GREEN) {
+            pins.analogWritePin(AnalogPin.P2, pins.map(0, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(255 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(0, 0, 1023, 0, 255))
+        }
+
+        if (led == LED.BLUE) {
+            pins.analogWritePin(AnalogPin.P2, pins.map(0, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(0, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(255 / intensity, 0, 1023, 0, 255))
+        }
+
+        if (led == LED.YELLOW) {
+            pins.analogWritePin(AnalogPin.P2, pins.map(255 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(255 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(0, 0, 1023, 0, 255))
+        }
+
+        if (led == LED.PINK) {
+            pins.analogWritePin(AnalogPin.P2, pins.map(255 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(192 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(203 / intensity, 0, 1023, 0, 255))
+        }
+
+        if (led == LED.PURPLE) {
+            pins.analogWritePin(AnalogPin.P2, pins.map(238 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(130 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(238 / intensity, 0, 1023, 0, 255))
+        }
+
+        if (led == LED.BROWN) {
+            pins.analogWritePin(AnalogPin.P2, pins.map(165 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(42 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(42 / intensity, 0, 1023, 0, 255))
+        }
+
+        if (led == LED.WHITE) {
+            pins.analogWritePin(AnalogPin.P2, pins.map(255 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(255 / intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(255 / intensity, 0, 1023, 0, 255))
+        }
     }
 
     //% blockId="blink_led" block="Blink LED %led| with period of %interval| ms and intensity level %intensity"
