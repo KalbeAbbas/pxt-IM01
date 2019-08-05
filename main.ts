140eefc87c76a9849f122c97a5abb3db66684e7f
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-05 19:29:35 +0500

    intensity added

--- main.ts
+++ main.ts
@@ -24,26 +24,27 @@ namespace BM11_led {
 
     //% blockId="blink_led" block="Blink LED %led| with period of %interval| ms and intensity %intensity"
     //% weight=30 blockGap=8
-    export function blink_led(led: LED, intensity:number, interval: number) {
+    //%intensity.min=0 intensity.max=10
+    export function blink_led(led: LED, intensity: number, interval: number) {
 
         if (led == LED.RED) {
-            pins.digitalWritePin(DigitalPin.P2, 1)
+            pins.analogWritePin(AnalogPin.P2, pins.map(255/intensity, 0, 1023, 0, 255))
             basic.pause(interval)
-            pins.digitalWritePin(DigitalPin.P2, 0)
+            pins.analogWritePin(AnalogPin.P2, pins.map(255/intensity, 0, 1023, 0, 255))
             basic.pause(interval)
         }
 
         if (led == LED.GREEN) {
-            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.analogWritePin(AnalogPin.P8, pins.map(255/intensity, 0, 1023, 0, 255))
             basic.pause(interval)
-            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.analogWritePin(AnalogPin.P8, pins.map(255/intensity, 0, 1023, 0, 255))
             basic.pause(interval)
         }
 
         if (led == LED.BLUE) {
-            pins.digitalWritePin(DigitalPin.P16, 1)
+            pins.analogWritePin(AnalogPin.P16, pins.map(255/intensity, 0, 1023, 0, 255))
             basic.pause(interval)
-            pins.digitalWritePin(DigitalPin.P16, 0)
+            pins.analogWritePin(AnalogPin.P16, pins.map(255/intensity, 0, 1023, 0, 255))
             basic.pause(interval)
         }
 
@@ -66,9 +67,9 @@ namespace BM11_led {
         }
 
         if (led == LED.PINK) {
-            pins.analogWritePin(AnalogPin.P2, pins.map(255, 0, 1023, 0, 255))
-            pins.analogWritePin(AnalogPin.P8, pins.map(192, 0, 1023, 0, 255))
-            pins.analogWritePin(AnalogPin.P16, pins.map(203, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P2, pins.map(255/intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(192/intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(203/intensity, 0, 1023, 0, 255))
             basic.pause(interval)
             pins.analogWritePin(AnalogPin.P2, pins.map(0, 0, 1023, 0, 255))
             pins.analogWritePin(AnalogPin.P8, pins.map(0, 0, 1023, 0, 255))
@@ -77,9 +78,9 @@ namespace BM11_led {
         }
 
         if (led == LED.PURPLE) {
-            pins.analogWritePin(AnalogPin.P2, pins.map(238, 0, 1023, 0, 255))
-            pins.analogWritePin(AnalogPin.P8, pins.map(130, 0, 1023, 0, 255))
-            pins.analogWritePin(AnalogPin.P16, pins.map(238, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P2, pins.map(238/intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(130/intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(238/intensity, 0, 1023, 0, 255))
             basic.pause(interval)
             pins.analogWritePin(AnalogPin.P2, pins.map(0, 0, 1023, 0, 255))
             pins.analogWritePin(AnalogPin.P8, pins.map(0, 0, 1023, 0, 255))
@@ -88,9 +89,9 @@ namespace BM11_led {
         }
 
         if (led == LED.BROWN) {
-            pins.analogWritePin(AnalogPin.P2, pins.map(165, 0, 1023, 0, 255))
-            pins.analogWritePin(AnalogPin.P8, pins.map(42, 0, 1023, 0, 255))
-            pins.analogWritePin(AnalogPin.P16, pins.map(42, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P2, pins.map(165/intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P8, pins.map(42/intensity, 0, 1023, 0, 255))
+            pins.analogWritePin(AnalogPin.P16, pins.map(42/intensity, 0, 1023, 0, 255))
             basic.pause(interval)
             pins.analogWritePin(AnalogPin.P2, pins.map(0, 0, 1023, 0, 255))
             pins.analogWritePin(AnalogPin.P8, pins.map(0, 0, 1023, 0, 255))
